af4e3ee8881afe21d33b602efa150792b56a4270
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-26 17:42:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class School
-  attr_accessor :roster
+  attr_accessor :roster, :add_student
   def initialize(school)
     @school = school
     @roster = {}
   end 
-   
+  
   
 end
 
